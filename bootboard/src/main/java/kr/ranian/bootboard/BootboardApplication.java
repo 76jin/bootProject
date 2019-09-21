0f586c99d049d167007065a309dc5d612781d259
ranian129 <ranian129@gmail.com>
2019-09-21 17:57:04 +0900

    add bootboard projecte

---
+++ bootboard/src/main/java/kr/ranian/bootboard/BootboardApplication.java
@@ -0,0 +1,13 @@
+package kr.ranian.bootboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootboardApplication.class, args);
+    }
+
+}
---
+++ bootboard/src/test/java/kr/ranian/bootboard/BootboardApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.ranian.bootboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootboardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
