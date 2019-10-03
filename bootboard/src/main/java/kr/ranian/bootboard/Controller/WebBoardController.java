f67ec74d17c56c410771e2555f0c435f8d879521
ranian129 <ranian129@gmail.com>
2019-10-03 19:23:06 +0900

    샘플용 게시판(WebBoard) 등록 기능 추가.

--- bootboard/src/main/java/kr/ranian/bootboard/Controller/WebBoardController.java
+++ bootboard/src/main/java/kr/ranian/bootboard/Controller/WebBoardController.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Created by ranian129@gmail.com on 2019-09-28
@@ -49,5 +51,21 @@ public class WebBoardController {
         log.info("Total Page Number:" + result.getTotalPages());
 
         model.addAttribute("result", new PageMaker<>(result));
+    }
+
+    @GetMapping("/register")
+    public void registerGet(@ModelAttribute("vo") WebBoard vo) {
+        log.info("register get");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@ModelAttribute("vo") WebBoard vo, RedirectAttributes rttr) {
+        log.info("register post");
+        log.info(" " + vo);
+
+        repo.save(vo);
+        rttr.addFlashAttribute("msg", "success");
+
+        return "redirect:/boards/list";
     }
 }
