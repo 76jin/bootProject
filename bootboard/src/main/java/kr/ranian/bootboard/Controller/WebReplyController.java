9c8e9e3746107ba42cdb9481fa414473e86c598a
ranian129 <ranian129@gmail.com>
2019-10-04 21:20:22 +0900

    샘플 댓글용 Controller 추가.

---
+++ bootboard/src/main/java/kr/ranian/bootboard/Controller/WebReplyController.java
@@ -0,0 +1,94 @@
+package kr.ranian.bootboard.Controller;
+
+import kr.ranian.bootboard.domain.WebBoard;
+import kr.ranian.bootboard.domain.WebReply;
+import kr.ranian.bootboard.persistence.WebReplyRepository;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by ranian129@gmail.com on 2019-10-04
+ * Blog : http://76jin.tistory.com
+ * Github : http://github.com/76jin
+ */
+@Log
+@RestController
+@RequestMapping("/replies/*")
+public class WebReplyController {
+
+    @Autowired  // setter를 만들어서 처리하는 것이 정석이지만..
+    private WebReplyRepository replyRepo;
+
+    @Transactional
+    @PostMapping("/{bno}")
+    public ResponseEntity<List<WebReply>> addReply(
+            @PathVariable("bno") Long bno,
+            @RequestBody WebReply reply) {
+        log.info("addReply........");
+        log.info("bno:" + bno);
+        log.info("Reply:" + reply);
+
+        WebBoard board = new WebBoard();
+        board.setBno(bno);
+
+        reply.setBoard(board);
+
+        replyRepo.save(reply);
+
+        return new ResponseEntity<>(getListByBoard(board), HttpStatus.CREATED);
+    }
+
+    private List<WebReply> getListByBoard(WebBoard board) throws RuntimeException {
+        log.info("getListByBoard..." + board);
+        return replyRepo.getRepliesObBoard(board);
+    }
+
+    @DeleteMapping("/{bno}/{rno}")
+    public ResponseEntity<List<WebReply>> remove(
+            @PathVariable("bno") Long bno,
+            @PathVariable("rno") Long rno) {
+        log.info("delete reply:" + rno);
+
+        replyRepo.deleteById(rno);
+
+        WebBoard board = new WebBoard();
+        board.setBno(bno);
+
+        return new ResponseEntity<>(getListByBoard(board), HttpStatus.OK);
+    }
+
+    @Transactional
+    @PutMapping("/{bno}x")
+    public ResponseEntity<List<WebReply>> modify(
+            @PathVariable("bno") Long bno,
+            @RequestBody WebReply reply) {
+        log.info("modify reply:" + reply);
+
+        replyRepo.findById(reply.getRno()).ifPresent(origin -> {
+            origin.setReplyText(reply.getReplyText());
+            replyRepo.save(origin);
+        });
+
+        WebBoard board = new WebBoard();
+        board.setBno(bno);
+
+        return new ResponseEntity<>(getListByBoard(board), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{bno}")
+    public ResponseEntity<List<WebReply>> getReplies(@PathVariable("bno") Long bno) {
+        log.info("get All Replies........");
+
+        WebBoard board = new WebBoard();
+        board.setBno(bno);
+
+        return new ResponseEntity<>(getListByBoard(board), HttpStatus.OK);
+    }
+
+}
--- bootboard/src/main/java/kr/ranian/bootboard/persistence/WebReplyRepository.java
+++ bootboard/src/main/java/kr/ranian/bootboard/persistence/WebReplyRepository.java
@@ -1,7 +1,11 @@
 package kr.ranian.bootboard.persistence;
 
+import kr.ranian.bootboard.domain.WebBoard;
 import kr.ranian.bootboard.domain.WebReply;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by ranian129@gmail.com on 2019-10-04
@@ -9,4 +13,7 @@ import org.springframework.data.repository.CrudRepository;
  * Github : http://github.com/76jin
  */
 public interface WebReplyRepository extends CrudRepository<WebReply, Long> {
+
+    @Query("select r from WebReply r where r.board = ?1 and r.rno > 0 order by r.rno asc ")
+    List<WebReply> getRepliesObBoard(WebBoard board);
 }
