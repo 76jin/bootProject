ec8b2b6c9fdd1ab5b872be3094126594b5defedd
ranian129 <ranian129@gmail.com>
2019-10-18 19:38:15 +0900

    시큐리티 에노테이션 샘플 추가.

--- bootsecurity/src/main/java/kr/ranian/bootsecurity/security/SecurityConfig.java
+++ bootsecurity/src/main/java/kr/ranian/bootsecurity/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,6 +24,7 @@ import javax.sql.DataSource;
  */
 @Log
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
